9d6d8dc80d678382f0a84684314d873caa72b898
Benarji <benarji69.bb@gmail.com>
2024-12-03 23:37:27 -0500

    linked upadted

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.prod.ts
 export const environment = {
-    production: true,
-    apiUrl: 'meanappbackend-production.up.railway.app'
-  };
+  production: true,
+  apiUrl: 'https://meanappbackend-production.up.railway.app/api'
+};
